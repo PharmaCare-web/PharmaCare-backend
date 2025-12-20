5031902ed0f6ba498db6d968fa5dad2ba5648985
dagmawit <dagmawitadeferes@gmail.com>
2025-12-21 01:07:43 +0300

    Remove duplicate inner backend folder

--- backend/middleware/Rolemiddelware.js
+++
@@ -1,9 +0,0 @@
-// middlewares/roleMiddleware.js
-export const permit = (...allowedRoles) => {
-  return (req, res, next) => {
-    const userRole = req.user?.role;
-    if (!userRole) return res.status(403).json({ message: "Role missing" });
-    if (allowedRoles.includes(userRole)) return next();
-    return res.status(403).json({ message: "Forbidden: insufficient permissions" });
-  };
-};
